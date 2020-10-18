db07328cabdb72b69a2cad3b4ee46672fa9cd971
Radhesh Sarma <radheshsarma29@gmail.com>
2020-10-18 22:33:00 +0530

    Update person_wise_seggregate.py

--- person_wise_seggregate.py
+++ person_wise_seggregate.py
@@ -10,5 +10,4 @@ ss = set([])
 
 for i in os.listdir(basepath):
     person_id = str(i[0] + i[1] + i[2] + i[3])
-    print(str(BASE_DIR) + '/' + basepath + '/' + i)
     move(str(BASE_DIR) + '/' + basepath + '/' + i,str(BASE_DIR) + '/' + basepath + '/' + person_id + '/')
